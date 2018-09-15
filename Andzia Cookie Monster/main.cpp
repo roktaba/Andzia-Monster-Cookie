be1f0c44559cc6fc6dd2f2ac52c27e98712b850f
roktaba <r.oktaba@gmail.com>
2018-09-15 18:20:18 +0200

    fullscreen

--- Andzia Cookie Monster/main.cpp
+++ Andzia Cookie Monster/main.cpp
@@ -8,7 +8,7 @@ int main()
 	unsigned int screenheight = 960;
 	enum gameState { MENU, GAME, GAME_CLOSE };
 	gameState state = MENU;
-	sf::RenderWindow window(sf::VideoMode(screenwidht, screenheight), "ANDZIA COOKIE MONSTER!", sf::Style::Titlebar | sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(screenwidht, screenheight), "ANDZIA COOKIE MONSTER!", sf::Style::Titlebar | sf::Style::Close | sf::Style::Fullscreen);
 
 	while (window.isOpen())
 	{
